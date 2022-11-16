cb10afb515d44994a6519f10f009b5c800208b27
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2022-11-16 15:46:50 -0300

    Estrutura visual - Estrutura da Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,25 @@
-import { styled } from '../styles';
+import Image from 'next/image';
+import { HomeContainer, Product } from '../styles/pages/home';
 
-const Button = styled('button', {
-    backgroundColor: '$green300',
-    borderRadius: 4,
-    border: 0,
-    padding: '4px 8px',
-
-    span: {
-        fontWeight: 'bold'
-    },
-
-    '&:hover': {
-        filter: 'brightness(0.8)'
-    }
-});
+import camiseta from '../assets/camisetas/1.png';
 
 export default function Home() {
     return (
-        <Button>
-            <span>Teste</span>
-            Enviar
-        </Button>
+        <HomeContainer>
+            <Product>
+                <Image src={camiseta} width={520} height={480} alt="" />
+                <footer>
+                    <strong>Camiseta X</strong>
+                    <span>R$ 79,90</span>
+                </footer>
+            </Product>
+            <Product>
+                <Image src={camiseta} width={520} height={480} alt="" />
+                <footer>
+                    <strong>Camiseta X</strong>
+                    <span>R$ 79,90</span>
+                </footer>
+            </Product>
+        </HomeContainer>
     );
 }
