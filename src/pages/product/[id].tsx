e31ac93fb2043faeb1501b1c75043387ba063ad5
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2022-11-21 15:44:37 -0300

    Produto & Checkout - SSG com parâmetro dinâmico

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,4 +1,4 @@
-import { GetStaticProps } from 'next';
+import { GetStaticPaths, GetStaticProps } from 'next';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
 import Stripe from 'stripe';
@@ -34,6 +34,13 @@ export default function Product({ product }: ProductProps) {
         </ProductContainer>
     );
 }
+
+export const getStaticPaths: GetStaticPaths = async () => {
+    return {
+        paths: [{ params: { id: 'prod_MomDwD8NCt8fJ1' } }],
+        fallback: false
+    };
+};
 
 export const getStaticProps: GetStaticProps<any, { id: string }> = async ({
     params
