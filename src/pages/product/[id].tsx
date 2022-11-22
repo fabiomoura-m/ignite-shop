4ae0f8cefdd043eac3d0c992663aac51d16679fd
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2022-11-22 11:10:41 -0300

    Produto & Checkout - Fallback do SSG

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,6 +1,5 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
 import Image from 'next/image';
-import { useRouter } from 'next/router';
 import Stripe from 'stripe';
 import { stripe } from '../../lib/stripe';
 import {
@@ -38,7 +37,7 @@ export default function Product({ product }: ProductProps) {
 export const getStaticPaths: GetStaticPaths = async () => {
     return {
         paths: [{ params: { id: 'prod_MomDwD8NCt8fJ1' } }],
-        fallback: false
+        fallback: 'blocking'
     };
 };
 
