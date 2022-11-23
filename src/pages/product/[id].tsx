c20c21d9bfc2a206e504254319c58453b5e284aa
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2022-11-23 09:57:20 -0300

    Trabalhando com SEO

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { GetStaticPaths, GetStaticProps } from 'next';
+import Head from 'next/head';
 import Image from 'next/image';
 import { useState } from 'react';
 import Stripe from 'stripe';
@@ -44,22 +45,32 @@ export default function Product({ product }: ProductProps) {
     }
 
     return (
-        <ProductContainer>
-            <ImageContainer>
-                <Image src={product.imageUrl} width={520} height={480} alt="" />
-            </ImageContainer>
-            <ProductDetails>
-                <h1>{product.name}</h1>
-                <span>{product.price}</span>
-                <p>{product.description}</p>
-                <button
-                    disabled={isCreatingCheckoutSession}
-                    onClick={handleBuyProduct}
-                >
-                    Comprar agora
-                </button>
-            </ProductDetails>
-        </ProductContainer>
+        <>
+            <Head>
+                <title>{product.name} | Ignite Shop</title>
+            </Head>
+            <ProductContainer>
+                <ImageContainer>
+                    <Image
+                        src={product.imageUrl}
+                        width={520}
+                        height={480}
+                        alt=""
+                    />
+                </ImageContainer>
+                <ProductDetails>
+                    <h1>{product.name}</h1>
+                    <span>{product.price}</span>
+                    <p>{product.description}</p>
+                    <button
+                        disabled={isCreatingCheckoutSession}
+                        onClick={handleBuyProduct}
+                    >
+                        Comprar agora
+                    </button>
+                </ProductDetails>
+            </ProductContainer>
+        </>
     );
 }
 
